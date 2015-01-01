3f8393cd0be15737a5f93b5c40221a667098aad6
barahilia <barahilia@gmail.com>
2015-01-01 21:01:16 +0200

    Moving data to the controller

---
+++ www/app.js
@@ -0,0 +1,33 @@
+
+angular.module("app", [])
+
+.controller("ctrl", function ($scope) {
+    $scope.records = [
+        {
+            date: "2014-09-23",
+            text: "Lorem Ipsum is simply dummy text of the printing and " +
+                "typesetting industry. Lorem Ipsum has been the industry's " +
+                "standard dummy text ever since the 1500s"
+        },
+        {
+            date: "2014-10-27",
+            text: "second"
+        },
+        {
+            date: "2014-12-31",
+            text: "third"
+        }
+    ];
+    
+    if($scope.records.length > 0) {
+        $scope.selected = $scope.records[0];
+    }
+    
+    $scope.select = function (record) {
+        $scope.selected = record;
+    };
+})
+
+;
+
+
