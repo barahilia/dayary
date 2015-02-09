eba48241b773694f64f64a81b4845eba574ffcaa
barahilia <barahilia@gmail.com>
2015-02-09 22:06:43 +0200

    Auto-lock at user inactivity

--- www/encryptionService.js
+++ www/encryptionService.js
@@ -1,7 +1,40 @@
-var encryptionService = function () {
+var encryptionService = function (
+    $window, $interval, $state
+) {
     var service = {};
     var locked = true;
+    var lastUserAction = new Date();
     var passphrase;
+
+    var lock = function () {
+        passphrase = undefined;
+        locked = true;
+        $state.go("settings");
+    };
+
+    // TODO: move $window.onblur here too
+    var updateUserAction = function () {
+        lastUserAction = new Date();
+    };
+
+    // TODO: extract to the lock service
+    // TODO: use moment.js for all date operations
+    // TODO: choose $window.onclick vs angular.element($window).bind vs
+    //       angular.element($window).on("click") vs ...
+    $window.onclick = updateUserAction;
+    $window.onkeypress = updateUserAction;
+
+    $interval(
+        function () {
+            var now = new Date();
+            //if ((now - lastUserAction) > 15 * 60 * 1000) { // 15 min
+            if ((now - lastUserAction) > 15 * 1000) { // 15 sec
+                lock();
+            }
+        },
+        5 * 1000 // 5 sec
+    );
+
 
     service.getPassphrase = function () {
         return passphrase;
