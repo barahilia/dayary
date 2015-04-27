edb48dc0f8d1a15775914f9d14dbb557e7e9bfc9
Ilia Barahovsky <barahilia@gmail.com>
2015-04-27 22:44:24 +0300

    List of records by year

--- www/yearsCtrl.js
+++ www/yearsCtrl.js
@@ -1,4 +1,18 @@
-var yearsCtrl = function ($scope) {
-    $scope.aaa = "bbb";
+var yearsCtrl = function ($scope, $http, errorService) {
+
+    $http.get("/api/records")
+        .success(function (records) {
+            var years = _.map(
+                records,
+                function (record) {
+                    return moment(record.created).year();
+                }
+            );
+
+            $scope.years = _.countBy(years);
+        })
+        .error(function () {
+            errorService.reportError("failure while loading records list");
+        });
 };
 
