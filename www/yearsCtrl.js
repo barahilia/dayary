5d5615dcfbf547ee16bd5f6cb0323f1819f37670
Ilia Barahovsky <barahilia@gmail.com>
2015-05-24 16:05:08 +0300

    Ability to select a year

--- www/yearsCtrl.js
+++ www/yearsCtrl.js
@@ -1,5 +1,18 @@
 var yearsCtrl = function ($scope, $http, errorService) {
 
+    $scope.selectMonth = function (month) {
+        // Find all its days
+        // Present them
+    };
+
+    $scope.selectYear = function (year) {
+        $scope.selectedYear = year;
+        console.log(year);
+        // Generate all months for this year
+        // Count days in each
+        // Select first non-empty month
+    };
+
     $http.get("/api/records")
         .success(function (records) {
             var years = _.map(
@@ -10,6 +23,10 @@ var yearsCtrl = function ($scope, $http, errorService) {
             );
 
             $scope.years = _.countBy(years);
+
+            if (_.some(years)) {
+                $scope.selectYear(_.min(years));
+            }
         })
         .error(function () {
             errorService.reportError("failure while loading records list");
