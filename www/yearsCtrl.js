e74ea30d1b4b79500e19d54bd297c99ab8b152ad
Ilia Barahovsky <barahilia@gmail.com>
2015-05-24 16:38:16 +0300

    Prepare to count and select by month

--- www/yearsCtrl.js
+++ www/yearsCtrl.js
@@ -1,5 +1,7 @@
 var yearsCtrl = function ($scope, $http, errorService) {
 
+    var records;
+
     $scope.selectMonth = function (month) {
         // Find all its days
         // Present them
@@ -9,12 +11,31 @@ var yearsCtrl = function ($scope, $http, errorService) {
         $scope.selectedYear = year;
         console.log(year);
         // Generate all months for this year
+        $scope.months = _.map(
+            moment.months(),
+            function (month, index) {
+                return {
+                    name: month,
+                    index: index,
+                    count: _.filter(
+                        records,
+                        function (record) {
+                            var date = moment(record.created);
+                            return date.year() == year && date,month() == index;
+                        }
+                    )
+                };
+            }
+        );
+
         // Count days in each
         // Select first non-empty month
     };
 
     $http.get("/api/records")
-        .success(function (records) {
+        .success(function (data) {
+            records = data;
+
             var years = _.map(
                 records,
                 function (record) {
