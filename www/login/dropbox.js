42572612e6497b3d679441fa9d6a2274d45c788f
Ilia Barahovsky <barahilia@gmail.com>
2015-07-23 22:37:18 +0300

    Working authentication at an independent page

---
+++ www/login/dropbox.js
@@ -0,0 +1,24 @@
+var client = new Dropbox.Client({ key: "4hxwutae96fhhbd" });
+
+client.authenticate({interactive: true}, function(error, client) {
+    var d;
+
+    if (error) {
+        console.log(error);
+
+        d = document.createElement("div");
+        d.innerHTML = "Authentication error - see console for details";
+        document.body.appendChild(d);
+
+        return;
+    }
+
+    if (client.isAuthenticated()) {
+        window.location.href = "/";
+    }
+    else {
+        d = document.createElement("div");
+        d.innerHTML = "Internal (Dropbox) error - not authenticated";
+        document.body.appendChild(d);
+    }
+});
