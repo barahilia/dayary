b26adaaf95c0b0cf4baaf78bc1784fed0820ec21
Ilia Barahovsky <barahilia@gmail.com>
2015-08-01 17:21:46 +0300

    Real mock and tests for the lockService

--- test/app-spec.js
+++ test/app-spec.js
@@ -1,7 +1,20 @@
 
 describe("lock service", function () {
-    
+
+    beforeEach(module('app'));
+
     it("should simply pass at last", function () {
         expect(true).toBeTruthy();
     });
+
+    it("should start locked", inject(function (lockService) {
+        expect(lockService.locked()).toBeTruthy();
+    }));
+
+    it("should be locked and unlocked", inject(function (lockService) {
+        lockService.unlock();
+        expect(lockService.locked()).toBeFalsy();
+        lockService.lock();
+        expect(lockService.locked()).toBeTruthy();
+    }));
 });
