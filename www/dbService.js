cddcd630b82b7ced80762d165bc0127f3ca96681
Ilia Barahovsky <barahilia@gmail.com>
2015-08-03 22:13:47 +0300

    Adding dummy db service

---
+++ www/dbService.js
@@ -0,0 +1,6 @@
+var dbService = function () {
+    var service = {};
+    console.log("db service init");
+    return service;
+};
+
--- www/runApp.js
+++ www/runApp.js
@@ -1,4 +1,4 @@
-var runApp = function ($rootScope, $state, lockService) {
+var runApp = function ($rootScope, $state, lockService, dbService) {
 
     $rootScope.$on('$stateChangeStart', function(e, to) {
         lockService.previousState($state.current.name, $state.params);
