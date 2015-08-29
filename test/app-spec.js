5e8bef3a1b058d26e7264353ed0301bb8a009e54
Ilia Barahovsky <barahilia@gmail.com>
2015-08-29 20:57:49 +0300

    Moving most server db tests to client tests

--- test/app-spec.js
+++ test/app-spec.js
@@ -79,11 +79,28 @@ describe("app module", function () {
                 });
         });
 
-        it("should set setting with no error", function (done) {
+        it("should set and get hash", function (done) {
+            service.setHash("aba")
+                .then(service.getHash)
+                .then(function (hash) {
+                    expect(hash).toBe("aba");
+                    done();
+                });
+        });
+
+        it("should set one setting", function (done) {
             service.setSettings({ a: 42 })
                 .then(function (results) {
                     expect(results.length).toBe(1);
                     expect(results[0].rowsAffected).toBe(1);
+                    done();
+                });
+        });
+
+        it("should get one setting now", function (done) {
+            service.getSettings()
+                .then(function (settings) {
+                    expect(settings).toEqual({ a: 42 });
                     done();
                 });
         });
@@ -95,6 +112,65 @@ describe("app module", function () {
                     done();
                 });
         });
+
+        it("should add a record", function (done) {
+            service.addRecord({text: 'aa', created: 1, updated: 2 })
+                .then(function (record) {
+                    expect(record).toEqual(
+                        { id: 1, text: 'aa', created: '1.0', updated: '2.0' }
+                    );
+                    done();
+                });
+        });
+
+        it("should update a record", function (done) {
+            service.updateRecord(
+                { id: 1, text: 'bbb', created: '1', updated: '15' }
+            ).then(function (data) {
+                expect(data).toBeUndefined();
+                done();
+            });
+        });
+
+        it("should add another record", function (done) {
+            service.addRecord({ created: 3, updated: 4 })
+                .then(function (record) {
+                    expect(record).toEqual(
+                        { id: 2, text: 'undefined', created: '3.0', updated: '4.0' }
+                    );
+                    done();
+                });
+        });
+
+        it("should have two records by now", function (done) {
+            service.getAllRecords()
+                .then(function (records) {
+                    expect(records).toEqual([
+                        { id: 1, created: '1', updated: '15' },
+                        { id: 2, created: '3.0', updated: '4.0' }
+                    ]);
+                    done();
+                });
+        });
+
+        it("should delete a record", function (done) {
+            service.deleteRecord(1)
+                .then(function (data) {
+                    expect(data).toBeUndefined();
+                    done();
+                });
+        });
+
+        it("should have one record after deletion", function (done) {
+            service.getAllRecords()
+                .then(function (records) {
+                    expect(records).toEqual([
+                        { id: 2, created: '3.0', updated: '4.0' }
+                    ]);
+                    done();
+                });
+        });
     });
 
 });
+
