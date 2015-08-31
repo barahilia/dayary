b44edc587d15efd45c66250421f8fd6829561347
Ilia Barahovsky <barahilia@gmail.com>
2015-08-31 20:43:02 +0300

    Cleanup unused npm packages and using default script for npm start

--- server.js
+++ server.js
@@ -4,20 +4,9 @@ var express = require('express');
 
 // TODO: describe configuration in README
 var port = process.env.PORT || 3000;
-
 var app = express();
 
-app.use(
-    '/bower_components',
-    express.static(__dirname + '/../bower_components')
-);
-app.use(
-    '/node_modules',
-    express.static(__dirname + '/../node_modules')
-);
-app.use(
-    express.static(__dirname + '/../www')
-);
-
+app.use(express.static(__dirname));
 console.log("Listening... Get to http://localhost:" + port);
 app.listen(port);
+
