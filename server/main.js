bd84bfe7a8d0e85e2d20088a209422b473d17ea8
Ilia Barahovsky <barahilia@gmail.com>
2015-08-31 20:18:17 +0300

    Removing server API and backend

--- server/main.js
+++ server/main.js
@@ -1,21 +1,11 @@
 #!/usr/bin/env node
 
-var express = require('express'),
-    backend = require('./sqlite-backend'),
-    recordsApi = require('./records-api'),
-    settingsApi = require('./settings-api');
+var express = require('express');
 
 // TODO: describe configuration in README
-// TODO: accept config from the command line
 var port = process.env.PORT || 3000;
-var dbFile = process.env.DBFILE || __dirname + "/../data/records.sqlite";
 
 var app = express();
-
-backend.openDb(dbFile);
-
-app.use('/api/records', recordsApi);
-app.use('/api/settings', settingsApi);
 
 app.use(
     '/bower_components',
