336b1ebd3e583eed346b9e6d4213b866ec959675
Ilia Barahovsky <barahilia@gmail.com>
2022-08-26 22:09:33 +0300

    Responsible banner menu

--- www/bannerCtrl.js
+++ www/bannerCtrl.js
@@ -1,14 +1,20 @@
 bannerCtrl = function ($scope, lockService, errorService) {
 
+    errorService.setCallback(function (error) {
+        $scope.error = error;
+    });
+
     $scope.lock = function () {
         lockService.lock();
     };
 
     $scope.locked = lockService.locked;
 
-    errorService.setCallback(function (error) {
-        $scope.error = error;
-    });
+    $scope.showMenu = false;
+
+    $scope.toggleMenu = function () {
+        $scope.showMenu = ! $scope.showMenu;
+    };
 
     $scope.unsetError = function () {
         $scope.error = null;
